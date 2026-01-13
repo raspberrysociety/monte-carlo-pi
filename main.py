976b64b6d03afe781c9647532703e8719ff27164
raspberrysociety <raspberrysociety@proton.me>
2026-01-13 19:51:44 -0300

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from calcular_pi import calcular_pi
 def main():
     n_pontos = int(input("Digite o número de pontos para a simulação: "))
 
-    pi_estimado, pontos, pontos_dentro = calcular_pi(n_pontos)
+    pi_estimado, pontos_dentro = calcular_pi(n_pontos)
 
     print(f"Pontos dentro do círculo: {pontos_dentro}")
     print(f"Total de pontos: {n_pontos}")
