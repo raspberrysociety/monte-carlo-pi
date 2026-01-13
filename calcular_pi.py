0a37fe0f6a656ae83f15828b7d2b3c631362d71c
raspberrysociety <raspberrysociety@proton.me>
2026-01-13 19:00:10 -0300

    calcular_pi.py

---
+++ calcular_pi.py
@@ -0,0 +1,16 @@
+import random
+
+def calcular_pi(n_pontos):
+    pontos_dentro = 0
+    pontos = []
+
+    for _ in range(n_pontos):
+        x = random.random()
+        y = random.random()
+        pontos.append((x, y))
+
+        if x**2 + y**2 <= 1:
+            pontos_dentro += 1
+
+    pi_estimado = 4 * pontos_dentro / n_pontos
+    return pi_estimado, pontos, pontos_dentro
